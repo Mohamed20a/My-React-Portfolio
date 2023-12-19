032b1f4deda43c28be1b2772cb7c82f7c77e2a18
Mohamed Ahmed <100856846+Mohamed20a@users.noreply.github.com>
2023-12-19 14:19:41 +0200

    Add files via upload

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import logo from '../../assets/img/coding.png';
 import "../../style/style.css";
 
 class Navbar extends Component {
@@ -30,42 +29,19 @@ class Navbar extends Component {
       <nav className="navbar navbar-expand-lg  ">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
-            <h2 style={{fontfamily: 'toyor-aljanah', color: 'rgb(131 219 255/1)'}}> Mohamed <img style={{ width: '35px' }} src="https://img.icons8.com/fluency/48/source-code.png" alt="شعار الموقع" /> </h2>
+            <h2 style={{fontfamily: 'toyor-aljanah', color: 'rgb(131 219 255/1)'}}> Mohamed <img style={{ width: '35px' }} src="https://img.icons8.com/fluency/48/source-code.png" alt="logo" /> </h2>
           </Link>
-          <button
-        className={`navbar-toggler ${isVisible ? "active" : ""}`}
-        type="button"
-        onClick={this.handleToggleOffcanvas}
-        aria-controls="offcanvasNavbar"
-        aria-label="Toggle navigation"
-      >
+          <button className={`navbar-toggler ${isVisible ? "active" : ""}`} type="button" onClick={this.handleToggleOffcanvas} aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
             <i className="fa-solid fa-bars-staggered"></i>
           </button>
-          <div
-            className={`offcanvas offcanvas-end ${isVisible ? 'show' : ''}`}
-            tabIndex="-1"
-            id="offcanvasNavbar"
-            aria-labelledby="offcanvasNavbarLabel"
-          >
+          <div className={`offcanvas offcanvas-end ${isVisible ? 'show' : ''}`} tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
             <div className="offcanvas-header">
-              <button
-                className="button1"
-                onClick={this.handleHideOffcanvas}
-                aria-label="Close"
-              >
-                {/* <span className="X"><i class="fa-solid fa-xmark"></i></span> */}
-                {/* <span className="Y"></span> */}
-                <i id="close" class="fa-solid fa-xmark"></i>
+              <button className="button1" onClick={this.handleHideOffcanvas} aria-label="Close">                
+              <i id="close" class="fa-solid fa-xmark"></i>
               </button>
             </div>
             <div className="offcanvas-body">
-            <ul
-                className="navbar-nav justify-content-end flex-grow-1 pe-3"
-                onClick={() => {
-                  this.handleHideOffcanvas();
-                  this.setState({ isBlurred: false });
-                }}
-              >
+            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3" onClick={() => {   this.handleHideOffcanvas();   this.setState({ isBlurred: false }); }}>
                 <li className="nav-item">
                   <Link className="nav-link" to="/" >
                     <span className="num">01</span>
